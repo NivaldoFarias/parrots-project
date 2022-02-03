4109ef3f8ae428c67778cbe68f1b75bdb154a057
Nivaldo Farias <badivia.f@gmail.com>
2022-02-03 16:39:39 -0300

    Added card-flip animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 let nCards = parseInt(prompt("Quantas cartas?"));
 
-for (let i = nCards; i > 0; i--){
-  let collection = document.querySelector("main");
+gameInit();
 
-  collection.innerHTML += `
-    <div class="card"><img src="dist/img/front.png" alt="papagaio"></div>
-  `;
+function gameInit(){
+  while (nCards < 4){
+    nCards = parseInt(prompt("Quantas cartas? 4-14"));
+  }
+  for (let i = nCards; i > 0; i--){
+    let collection = document.querySelector("main");
+
+    collection.innerHTML += `
+      <div class="card">
+        <div class="front-face face"><img src="dist/img/front.png" alt="papagaio"></div>
+        <div class="back-face face"><img src="dist/img/bobrossparrot.gif" alt="papagaio bob ross dancando"></div>
+      </div>
+    `;
+  }
 }
-
