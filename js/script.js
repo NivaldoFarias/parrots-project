fda643e0bfc5260e7200d14dce24012da232104e
Nivaldo Farias <badivia.f@gmail.com>
2022-02-04 14:49:51 -0300

    Added card-flip function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
 let nCards = parseInt(prompt("Quantas cartas?"));
 
 gameInit();
+rotate();
 
 function gameInit(){
   while (nCards < 4){
     nCards = parseInt(prompt("Quantas cartas? 4-14"));
   }
   for (let i = nCards; i > 0; i--){
-    let collection = document.querySelector("main");
+    let cards = document.querySelector("main");
 
-    collection.innerHTML += `
+    cards.innerHTML += `
       <div class="card">
-        <div class="front-face face"><img src="dist/img/front.png" alt="papagaio"></div>
-        <div class="back-face face"><img src="dist/img/bobrossparrot.gif" alt="papagaio bob ross dancando"></div>
+        <div class="front-face face">
+          <img src="dist/img/front.png" alt="papagaio">
+        </div>
+        <div class="back-face face">
+          <img src="dist/img/bobrossparrot.gif" alt="papagaio bob ross dancando">
+        </div>
       </div>
     `;
   }
+}
+function rotate(){
+  const cards = Array.from(document.getElementsByClassName("card"));
+
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('visible');
+    })
+  })
 }
