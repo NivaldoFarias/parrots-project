ef4acdfefdb92eb497b0b8d0c8366045ae0275c4
Nivaldo Farias <badivia.f@gmail.com>
2022-02-04 15:51:48 -0300

    Added total card-flip counter

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 let nCards = parseInt(prompt("Quantas cartas?"));
+let flips = 0;
+const gameIndex = document.querySelector("section p");
 
 gameInit();
 rotate();
 
 function gameInit(){
-  while (nCards < 4){
-    nCards = parseInt(prompt("Quantas cartas? 4-14"));
+  while (nCards < 4 || (nCards % 2) != 0){
+    nCards = parseInt(prompt("Quantas cartas? 4-14 e n pode ser impar"));
   }
-  for (let i = nCards; i > 0; i--){
+  for (let i = nCards/2, j = 0; i > 0; i--, j++){
     let cards = document.querySelector("main");
+    const cardTypes = [
+      'dist/img/bobrossparrot.gif',
+      'dist/img/explodyparrot.gif',
+      'dist/img/fiestaparrot.gif',
+      'dist/img/metalparrot.gif',
+      'dist/img/revertitparrot.gif',
+      'dist/img/tripletsparrot.gif',
+      'dist/img/unicornparrot.gif'
+    ]
 
     cards.innerHTML += `
       <div class="card">
@@ -16,7 +27,15 @@ function gameInit(){
           <img src="dist/img/front.png" alt="papagaio">
         </div>
         <div class="back-face face">
-          <img src="dist/img/bobrossparrot.gif" alt="papagaio bob ross dancando">
+          <img src="${cardTypes[j]}" alt="papagaio dancando">
+        </div>
+      </div>
+      <div class="card">
+        <div class="front-face face">
+          <img src="dist/img/front.png" alt="papagaio">
+        </div>
+        <div class="back-face face">
+          <img src="${cardTypes[j]}" alt="papagaio dancando">
         </div>
       </div>
     `;
@@ -28,6 +47,11 @@ function rotate(){
   cards.forEach((card) => {
     card.addEventListener('click', () => {
       card.classList.toggle('visible');
+      updateIndex();
     })
   })
 }
+function updateIndex(){
+  gameIndex.innerText = `Flips: ${++flips}`;
+}
+
